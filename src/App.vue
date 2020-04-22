f06973abf70bace4c72c11588797e2a4655499a7
Manisha <manisha@shippable.com>
2020-04-21 21:59:10 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div>
       <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#/">SwampUp 2019</b-navbar-brand>
+        <b-navbar-brand href="#/">SwampUp 2020</b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
